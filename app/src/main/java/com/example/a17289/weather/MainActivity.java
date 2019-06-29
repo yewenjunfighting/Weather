761c432215433f1bb9822b36eff878af0c86b7a1
wenjun ye <1728914873@qq.com>
2019-06-29 12:23:33 +0800

    init project

--- app/src/main/java/com/example/a17289/weather/MainActivity.java
+++ app/src/main/java/com/example/a17289/weather/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // 加载weather_info布局
+        setContentView(R.layout.weather_info);
     }
 }
